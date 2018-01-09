530406afca0b6eb7cea23f22d8f36778d2fa1ac7
Joseph Bailey <35010131+LubeyG@users.noreply.github.com>
2018-01-09 15:38:33 +0000

    Back at home again lol

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,56 @@
 var slideNumber = 0;
+var modal;
+var img;
+var modalImg;
+var captionText;
+var span;
+
 
 window.onload = function() {
-	init();
+	modal = document.getElementById('myModal');
+	modalImg = document.getElementById("img01");
+	captionText = document.getElementById("caption");
+	span = document.getElementsByClassName("close")[0];
+	
 	showSlides(slideNumber);
-}
+	
+	$('.myImg').click(function() {
+		console.log("cock");
+		modal.style.display = "block";
+		modalImg.src = this.src;
+		captionText.innerHTML = this.alt;
 
-function init() {
-	console.log("init fired");
-}
+	});
+
+	span.onclick = function() { 
+	modal.style.display = "none";
+	};
+};
 
 function showSlides() {
-    var i;
+    try {
+		var i;
     var slides = document.getElementsByClassName("gallerySlides");
     var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";
     }
     slideNumber++;
-    if (slideNumber > slides.length) {slideNumber = 1}
+    if (slideNumber > slides.length) {
+		slideNumber = 1;
+	}
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideNumber - 1].style.display = "block";
     dots[slideNumber - 1].className += " active";
     setTimeout(showSlides, 10000);
+	}
+	catch(err) {
+		
+		console.log("i dun fucked up and made an error. but morgan put it in a try catch so it doesnt fuck up the rest of the stuff.");
+		
+		}
 }
 
 jQuery(document).ready(function($) {
@@ -31,3 +58,8 @@ jQuery(document).ready(function($) {
         window.location = $(this).data("href");
     });
 });
+
+
+
+
+
